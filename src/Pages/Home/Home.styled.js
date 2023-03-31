c731036d3b9ac6d1921f9c7b63bf7f9a36678b3e
Hnatiak <romanhnatiak@gmail.com>
2023-03-31 19:24:22 +0200

    Search film

---
+++ src/Pages/Home/Home.styled.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const MovieLink = styled(Link)`
+  text-decoration: none;
+  padding: 4px 15px;
+  &:hover {
+    border-radius: 15px;
+    box-shadow: 1px 2px 4px 3px rgba(0.5, 0, 0, 0.5);
+  }
+`;
+export const MovieList = styled.ul`
+  padding: 20px 0 20px;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`;
+
+export const Title = styled.h2`
+  margin-top: 40px;
+  margin-bottom: 0;
+`;
